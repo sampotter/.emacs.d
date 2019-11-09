65d0127d8f3fa6f5d10e6015f86d168fe280c0bd
Sam Potter <sfp@umiacs.umd.edu>
2019-11-08 20:35:20 -0500

    latest changes

--- sfp/sfp-auctex.el
+++ sfp/sfp-auctex.el
@@ -7,5 +7,11 @@
 
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
+(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+	  TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view)))
+
+(add-hook 'TeX-after-compilation-finished-functions
+          #'TeX-revert-document-buffer)
+
 (provide 'sfp-auctex)
 ;;; sfp-auctex.el ends here
