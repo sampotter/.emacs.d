58a5c6499ed8751675cf9b060d1e6143f58584db
Samuel Potter <sfp@umiacs.umd.edu>
2020-03-16 10:26:04 -0400

    prettifying org-mode

--- sfp/sfp-org-mode.el
+++ sfp/sfp-org-mode.el
@@ -5,6 +5,7 @@
 ;;; Code:
 
 (install-package-if-necessary 'htmlize)
+(install-package-if-necessary 'org-bullets)
 
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -30,6 +31,50 @@
 
 (setq org-mobile-inbox-for-pull "~/org/flagged.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
+(setq org-hide-emphasis-markers t)
+
+(font-lock-add-keywords
+ 'org-mode
+ '(("^ *\\([-]\\) "
+	(0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(let* ((variable-tuple
+        (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+              ((x-list-fonts "Verdana")         '(:font "Verdana"))
+              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+       (base-font-color     (face-foreground 'default nil 'default))
+       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  (custom-theme-set-faces
+   'user
+   `(org-level-8 ((t (,@headline ,@variable-tuple))))
+   `(org-level-7 ((t (,@headline ,@variable-tuple))))
+   `(org-level-6 ((t (,@headline ,@variable-tuple))))
+   `(org-level-5 ((t (,@headline ,@variable-tuple))))
+   `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+   `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+   `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+   `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(custom-theme-set-faces
+ 'user
+ '(variable-pitch
+   ((t (:family "Source Sans Pro" :height 160 :weight regular)))))
+
+(add-hook 'org-mode-hook 'variable-pitch-mode)
+(add-hook 'org-mode-hook 'visual-line-mode)
+
+(setq-default org-format-latex-options
+			  '(:foreground default
+				:background default
+				:scale 1.3
+				:matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
 
 (require 'ox-md)
 
